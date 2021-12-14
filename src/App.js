6af5a43ed5b84df4633d41fe13b63011eb045c3a
Pavel <browser091@gmail.com>
2021-12-14 23:27:00 +0300

    'addroutes'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
+import React from "react";
+
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Settings from "./components/Settings/Settings";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
 
 function App() {
-   return (
-      <div className="app-wrapper">
-         <Header />
-         <Navbar />
-         <Profile />
-      </div>
-   );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="content">
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import s from './Navbar.module.css'
 
 
 const Navbar = () => {
-  return (
-    <nav className={s.nav}>
-      <div><a href="#">Profile</a></div>
-      <div><a href="#">Mesages</a></div>
-      <div><a href="#">News</a></div>
-      <div><a href="#">Music</a></div>
-      <div><a href="#">Seattings</a></div>
+    return (
+        <nav className={s.nav}>
+            <div><a href="/profile">Profile</a></div>
+            <div><a href="/dialogs">Messages</a></div>
+            <div><a href="/news">News</a></div>
+            <div><a href="/music">Music</a></div>
+            <div><a href="/settings">Settings</a></div>
 
 
-    </nav>
-  )
+        </nav>
+    )
 }
 export default Navbar
